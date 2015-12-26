9563752041f5851761a705b1dc36e97d9c2b9697
waytoalpit <waytoalpit@gmail.com>
2015-12-26 00:46:11 -0500

    Adding properties file and Connection Utility class

--- src/com/crowdsourcing/DeviceInfoDao.java
+++ src/com/crowdsourcing/DeviceInfoDao.java
@@ -1,43 +1,52 @@
 package com.crowdsourcing;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 import java.util.logging.Logger;
 
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
-import com.mongodb.MongoClient;
 
 public class DeviceInfoDao {
 
-	Logger log = Logger.getLogger("Alpit");
+	Logger log = Logger.getLogger("DeviceInfoDao");
+	Properties prop = null;
+
+	DB db = null;
+	List<Device> data = null;
+	Connection conn = null;
+
+	public DeviceInfoDao() {
+
+		prop = new Properties();
+		try {
+			prop.load(getClass().getResourceAsStream("/com/resources/config.properties"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		conn = new Connection();
+	}
 
 	public List<Device> getAllDevices() {
-
-		MongoClient mongoClient = null;
-		DB db = null;
-		List<Device> data = null;
-
 		try {
 
+			db = conn.connectMongoDB();
+			conn.authenticateDB();
+
 			data = new ArrayList<Device>();
-			mongoClient = new MongoClient("XXX.XXX.XXX.XXX", 27017);
 
-			db = mongoClient.getDB("SpecsenseDB");
-			System.out.println("Connect to database successfully");
+			String deviceCollection = prop.getProperty("deviceCollection");
+			DBCollection coll = db.getCollection(deviceCollection);
 
-			boolean auth = db.authenticate("XXXXX", "XXXXX".toCharArray());
-			System.out.println("Authentication: " + auth);
-
-			DBCollection coll = db.getCollection("UE_Measurements");
-
-			System.out
-					.println("Collection UE_Measurements selected successfully");
+			log.info("Collection UE_Measurements selected successfully");
 
 			DBCursor cursor = coll.find();
-			log.info("Data getting retrived...");
+			log.info("Retrieving data");
 
 			while (cursor.hasNext()) {
 				DBObject o = cursor.next();
@@ -47,20 +56,19 @@ public class DeviceInfoDao {
 				device.setLast_scanned((String) o.get("last_scanned"));
 				device.setMac((String) o.get("mac"));
 				device.setUe_battery_power((String) o.get("ue_battery_power"));
-				device.setUe_channel_scanned((Integer) o
-						.get("ue_channel_scanned"));
-				device.setUe_channel_scanned_power((Double) o
-						.get("ue_channel_scanned_power"));
+				device.setUe_channel_scanned((Integer) o.get("ue_channel_scanned"));
+				device.setUe_channel_scanned_power((Double) o.get("ue_channel_scanned_power"));
 				DBObject dbObject = (DBObject) o.get("loc");
 
 				Location location = new Location();
 				location.setType((String) dbObject.get("type"));
-				location.setCoordinates((List<Double>) dbObject
-						.get("coordinates"));
+				location.setCoordinates((List<Double>) dbObject.get("coordinates"));
 				device.setLoct(location);
 
 				data.add(device);
 			}
+
+			log.info("data retrieved successfully");
 
 		} catch (Exception e) {
 			System.out.println("Error occured");
@@ -69,8 +77,9 @@ public class DeviceInfoDao {
 
 		finally {
 			db.cleanCursors(true);
-			mongoClient.close();
+			conn.mongoClient.close();
 		}
 		return data;
 	}
+
 }
